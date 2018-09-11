b59bfc6b4c7372162e3411a361469175bbb33554
Gabriel Simas <gabrielsimas@gmail.com>
2018-09-11 05:47:03 -0300

    Adicao dos Value Objects e retirada de Fluent Validations para serem colocados posteriormente.

---
+++ Simasoft.LojaWebModerna.Dominio/ObjetosDeValor/Email.cs
@@ -0,0 +1,18 @@
+﻿using Flunt.Validations;
+
+namespace Simasoft.LojaWebModerna.Dominio.ObjetosDeValor
+{
+    public class Email
+    {
+        protected Email() { }
+        public Email(string endereco)
+        {
+            Endereco = endereco;
+
+            /* new ValidationContract<Email>(this)
+                .IsEmail(x => x.Address, "E-mail inválido"); */
+        }
+
+        public string Endereco { get; private set; }
+    }
+}
