9f2a4da07344c35bf14c42f893a6d0a9ee32bde3
Gabriel Simas <gabrielsimas@gmail.com>
2018-09-23 16:15:15 -0300

    Artefatos corrigidos

--- Simasoft.LojaWebModerna.KernelCompartilhado/Comandos/IResultadoComando.cs
+++ Simasoft.LojaWebModerna.KernelCompartilhado/Comandos/IResultadoComando.cs
@@ -1,7 +1,8 @@
 namespace Simasoft.LojaWebModerna.KernelCompartilhado.Comandos
 {
+    //Arriar a oferenda para o compilador do dotnet
     public interface IResultadoComando
     {
-         
+        void oferenda(); 
     }
 }
